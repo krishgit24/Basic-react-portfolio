e250356d1ab2cbf05ac6b80fd832a3ae21bdfe53
Krish Dambir <krishdambir2718@gmail.com>
2024-12-10 00:24:50 +0530

    Accessible links in footer

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -8,17 +8,17 @@ const Footer = () => {
       <div className="icons3">
         <div className="contactway">
           <i className="fa fa-linkedin">
-            <span className="names">Linkedin</span>
+            <span className="names"><a href="https://www.linkedin.com/in/krish-dambir-1701a2289/" target="main">Linkedin</a></span>
           </i>
         </div>
         <i className="fa fa-twitter">
-          <span className="names">Twitter</span>
+          <span className="names"><a href="https://x.com/Krish_dambir_27" target="main">Twitter</a></span>
         </i>
         <span className="names">@send Email</span>
       </div>
       <div className="iconsnext">
         <i className="fa fa-github">
-          <span className="names">Github</span>
+          <span className="names"><a href="https://github.com/krishgit24" target="main">Github</a></span>
         </i>
         <i className="fa fa-mobile">
           <span className="names">Call</span>
